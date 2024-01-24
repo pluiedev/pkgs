f7ce559f02f49f42c012eaa4528772833e0dc19a
Leah Amelia Chen <hi@pluie.me>
2024-01-24 22:08:54 +0100

    add overlay

--- flake.nix
+++ flake.nix
@@ -6,20 +6,21 @@
   outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux"];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (sys: f nixpkgs.legacyPackages.${sys});
+
+    mkPackages = pkgs: let
+      inherit (pkgs.lib) pipe mapAttrs filterAttrs;
+    in
+      pipe ./. [
+        builtins.readDir
+        (mapAttrs
+          (n: ty:
+            if ty == "directory"
+            then pkgs.callPackage ./${n} {}
+            else null))
+        (filterAttrs (_: v: v != null))
+      ];
   in {
-    packages = forAllSystems (
-      pkgs: let
-        inherit (pkgs.lib) pipe mapAttrs filterAttrs;
-      in
-        pipe ./. [
-          builtins.readDir
-          (mapAttrs
-            (n: ty:
-              if ty == "directory"
-              then pkgs.callPackage ./${n} {}
-              else null))
-          (filterAttrs (_: v: v != null))
-        ]
-    );
+    packages = forAllSystems mkPackages;
+    overlays.default = _: mkPackages;
   };
 }
